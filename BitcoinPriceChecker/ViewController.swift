11743e3981c6a606dc5c9588b82c1fbb684bd4e1
KOTARO SUTO <ng1a5ss43@gmail.com>
2019-01-15 01:25:20 +0900

    Initial Commit

---
+++ BitcoinPriceChecker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitcoinPriceChecker
+//
+//  Created by Kotaro Suto on 2019/01/15.
+//  Copyright © 2019年 Kotaro Suto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
